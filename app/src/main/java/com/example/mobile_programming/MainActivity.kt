8a82c91a4e56ed04319c1484f27ee7680eb3bfe7
alifiathiqa <alifaathiqaerlin@gmail.com>
2023-10-02 08:40:25 +0700

    done

--- app/src/main/java/com/example/mobile_programming/MainActivity.kt
+++ app/src/main/java/com/example/mobile_programming/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobile_programming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,16 +14,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var input_tinggi : EditText
     private lateinit var btn : Button
     private lateinit var teks_hasil : TextView
+
+    companion object{
+        private const val STATE_RESULT = "state_result"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        input_lebar= findViewById(R.id.edt_width)
-        input_panjang= findViewById(R.id.edt_length)
-        input_tinggi= findViewById(R.id.edt_height)
-        btn= findViewById(R.id.btn_result)
-        teks_hasil= findViewById(R.id.result)
+        input_lebar = findViewById(R.id.edt_width)
+        input_panjang = findViewById(R.id.edt_length)
+        input_tinggi = findViewById(R.id.edt_height)
+        btn = findViewById(R.id.btn_result)
+        teks_hasil = findViewById(R.id.result)
 
         btn.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            teks_hasil.text = savedInstanceState.getString(STATE_RESULT)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, teks_hasil.text.toString())
     }
 
     override fun onClick(v: View?) {
